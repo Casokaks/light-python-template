8cb91afafcb81d65e4dc43227a726355fb1530b8
Andrea <casokaks@gmail.com>
2021-08-15 14:09:16 +0000

    Add template folders and files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# light-python-template
-Python repository template, light version
+# Project name
+
+Project description
+
+To install open a terminal and run the command: "pip install git+https://github.com/Casokaks/project-name.git"
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Package setup
+==================================
+
+Author: Casokaks (https://github.com/Casokaks/)
+Created on: Aug 15th 2021
+
+"""
+
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='projectname',
+    version='0.0.1',
+    author='Casokaks',
+    author_email='casokaks@gmail.com',
+    description='Light python template',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Casokaks/light-python-template',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Casokaks/light-python-template/issues"
+    },
+    license='MIT',
+    packages=['light-python-template'],
+    install_requires=[],  # ['plotly', 'numpy', ],
+)
+
