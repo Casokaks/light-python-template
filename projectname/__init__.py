527ea7233bec1dae4ccfa55ebaafd3dae89953bc
Casokaks <casokaks@gmail.com>
2021-08-20 13:54:21 +0000

    Update init

--- projectname/__init__.py
+++ projectname/__init__.py
@@ -14,8 +14,8 @@ Created on: Aug 15th 2021
 __version__ = "0.3.1"
 
 # -----------------------------------------------------------------------------------
-# Imports from modules
+# Imports ()
 # -----------------------------------------------------------------------------------
 from .module import *  # The General Store: expose all modules, functions, attributes in projectname
 from .module import placeholder  # The Convenience Store: expose only selected objects in projectname 
-import projectname.module  # Online grocery shopping: expose only modules in projectname 
+import projectname.module  # [RECOMMENDED] Online grocery shopping: expose only modules in projectname 
